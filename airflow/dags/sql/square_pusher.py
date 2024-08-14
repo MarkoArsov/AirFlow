93180935249786594866b2d642c35556d10a6076
Marko_Gjorgjievski_201136 <marko.gjorgjievski@students.finki.ukim.mk>
2024-08-14 19:28:17 +0200

    Updated push_square file

--- airflow/dags/sql/square_pusher.py
+++ airflow/dags/sql/square_pusher.py
@@ -1,7 +1,12 @@
 import pandas as pd
-
+from sqlalchemy import create_engine
 
 def push_square(**kwargs):
     ti = kwargs['ti']
     cleaned_data = ti.xcom_pull(task_ids='clean_square', key='cleaned_data')
     df = pd.DataFrame.from_dict(cleaned_data)
+
+    engine = create_engine('postgresql+psycopg2://airflow:airflow@localhost:54320/airflow')
+
+    df.to_sql('Property', engine, schema='RealEstate', if_exists='replace', index=False)
+    engine.dispose()
