9ce15ed2631a98df38cffddf5a31764be0ceda81
Marko Arsov <mar@mca.dev>
2024-09-16 18:52:50 +0200

    Added delta to dag

--- airflow/dags/real_estate_dag.py
+++ airflow/dags/real_estate_dag.py
@@ -9,6 +9,9 @@ from sql.novel_pusher import push_novel
 from scrape.square_scraper import scrape_square
 from clean.square_cleaner import clean_square
 from sql.square_pusher import push_square
+from scrape.delta_scraper import scrape_delta
+from clean.delta_cleaner import clean_delta
+from sql.delta_pusher import push_delta
 
 dag = DAG(
     'real_estate_pipeline',
@@ -51,7 +54,7 @@ scrape_square_task = PythonOperator(
 
 clean_square_task = PythonOperator(
     task_id='clean_square',
-    python_callable=clean_square(),
+    python_callable=clean_square,
     provide_context=True,
     dag=dag
 )
@@ -63,5 +66,27 @@ push_square_task = PythonOperator(
     dag=dag
 )
 
+scrape_delta_task = PythonOperator(
+    task_id='scrape_delta',
+    python_callable=scrape_delta,
+    provide_context=True,
+    dag=dag
+)
+
+clean_delta_task = PythonOperator(
+    task_id='clean_delta',
+    python_callable=clean_delta,
+    provide_context=True,
+    dag=dag
+)
+
+push_delta_task = PythonOperator(
+    task_id='push_delta',
+    python_callable=push_delta,
+    provide_context=True,
+    dag=dag
+)
+
 scrape_novel_task >> clean_novel_task >> push_novel_task
 scrape_square_task >> clean_square_task >> scrape_square_task
+scrape_delta_task >> clean_delta_task >> push_delta_task
